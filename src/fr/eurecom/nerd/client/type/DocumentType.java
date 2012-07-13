5dff6f88a7ab51c294cba878095bdade009a64d1
Giuseppe Rizzo <giuse.rizzo@gmail.com>
2012-07-13 14:13:52 +0200

    add type enums

---
+++ src/fr/eurecom/nerd/client/type/DocumentType.java
@@ -0,0 +1,21 @@
+//   nerd4java - A java library which provides a programmable interface to NERD
+//               http://nerd.eurecom.fr
+//
+//   Copyright 2012
+//
+//   Authors:
+//      Giuseppe Rizzo <giuse.rizzo@gmail.com>
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License published by
+// the Free Software Foundation, either version 3 of the License, or (at 
+// your option) any later version. See the file Documentation/GPL3 in the
+// original distribution for details. There is ABSOLUTELY NO warranty.
+
+
+package fr.eurecom.nerd.client.type;
+
+public enum DocumentType {
+    PLAINTEXT,
+    TIMEDTEXT
+}
